c9781e2cd729f04badd0e314bbcbbdcbb2318a47
Stan H <deindall@outlook.com>
2016-01-17 12:53:55 +0100

    Commented PathFinder (and tweaked a value)

--- src/path/pathfinder.cpp
+++ src/path/pathfinder.cpp
@@ -7,12 +7,12 @@ PathFinder::PathFinder()
 	: mContext(nullptr), mUseAStar(true) {}
 
 PathFinder::~PathFinder() {
-	if (mContext != nullptr)
+	if (mContext)
 		delete mContext;
 }
 
 Vec2i PathFinder::getStartPos() const {
-	if (mContext==nullptr)
+	if (!mContext)
 		return Vec2i(-1, -1);
 
 	PathContext::Node* node = mContext->getStart();
@@ -41,7 +41,7 @@ void PathFinder::computePath(PathContext* context) {
 }
 
 void PathFinder::startPathfind(PathContext* context) {
-	if (mContext != nullptr)
+	if (mContext)
 		delete mContext;
 	mContext = context;
 
@@ -70,15 +70,22 @@ void PathFinder::generatePath() {
 	PathContext::Node* startNode = mContext->getStart();
 	PathContext::Node* endNode = mContext->getEnd();
 
+	// We build the path by iterating through the nodes backwards.
+	// That is why the first pointed node is the end node.
+
 	PathContext::Node* node = endNode;
 
+	// If we didn't find the end node, we will build the path from the
+	// next node to check.
 	if (!node->previousDirection.isValid())
 		node = getWantedUncheckedNode();
-	if (node==nullptr)
+
+	if (!node)
 		node = startNode;
 
 	mPathEndNode = node;
 
+	// Once we have the last node, we start iterating.
 	while (node != startNode) {
 		mPath.push_front(node->previousDirection);
 		mContext->getStats().pathLength += getDirectionStepLength(node->previousDirection);
@@ -87,11 +94,13 @@ void PathFinder::generatePath() {
 		node = mContext->get(node->x+opp.xOffset, node->y+opp.yOffset);
 	}
 
+	// This computes the minimal distance the path is to the end node.
 	mContext->getStats().pathMinLength = mContext->getStats().pathLength +
 										 mContext->distancePredict(mPathEndNode, endNode);
 }
 
 PathContext::Node* PathFinder::getWantedUncheckedNode() {
+	// This is the only thing that changes between A* and Dijkstra.
 	return (mUseAStar ? getBestUncheckedNode() : getCloserUncheckedNode());
 }
 
@@ -99,7 +108,7 @@ PathContext::Node* PathFinder::getCloserUncheckedNode() {
 	PathContext::Node* curNode = nullptr;
 
 	for (PathContext::Node* node : mUncheckedNodes) {
-		if (curNode==nullptr)
+		if (!curNode)
 			curNode = node;
 		else if (node->stepCount < curNode->stepCount)
 			curNode = node;
@@ -127,17 +136,19 @@ void PathFinder::checkNode(PathContext::Node* node) {
 		Vec2i nodePos(node->x+dir.xOffset, node->y+dir.yOffset);
 
 		if (dir.opposite()==node->previousDirection)
-			continue;
+			continue; // This node is where we came from.
 		else if (!mContext->inMap(nodePos.x, nodePos.y))
-			continue;
+			continue; // This node does not exist (out of bounds).
 		else if (mContext->isWall(node->x, node->y, dir))
-			continue;
+			continue; // This direction is not valid for a path (wall).
 
 		PathContext::Node* n = mContext->get(nodePos.x, nodePos.y);
 		unsigned int stepValue = getDirectionStepLength(dir);
 
+		// We will change the node if it is worth it (the new path is shorter).
+		// Note that the starting value of stepCount is UINT_MAX (close to infinity).
 		if (node->stepCount+stepValue < n->stepCount) {
-			if (!n->previousDirection.isValid()) // means has not been added yet
+			if (!n->previousDirection.isValid()) // means has not been added yet.
 				mUncheckedNodes.push_back(n);
 
 			n->stepCount = node->stepCount+stepValue;
@@ -150,5 +161,6 @@ void PathFinder::checkNode(PathContext::Node* node) {
 }
 
 unsigned int PathFinder::getDirectionStepLength(Direction dir) {
-	return (dir.isCardinal() ? 100 : 144);
+	// 141 ~= 100*sqrt(2), the distance for a diagonal segment.
+	return (dir.isCardinal() ? 100 : 141);
 }
